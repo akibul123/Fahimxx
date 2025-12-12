1ddb85548d11996c722cb9cb11c6cd70b27101ad
Fahim Islam <160403765+akibul123@users.noreply.github.com>
2025-12-12 22:19:15 +0600

    Update 𝗪𝗔𝗥𝗡𝗜𝗡𝗚.js

--- scripts/commands/𝗪𝗔𝗥𝗡𝗜𝗡𝗚.js
+++ scripts/commands/𝗪𝗔𝗥𝗡𝗜𝗡𝗚.js
@@ -28,7 +28,7 @@ module.exports.handleEvent = async ({ event, api }) => {
     }
   }
 
-  const sensitiveKeywords = ["bal", "cudi", "sala abal", "sala", "fuck you", "xudi", "abal", " আবাল"]; // Dagdagan mo lang kung gusto mong maraming keywords 
+  const sensitiveKeywords = ["bal", "cudi", "sala abal", "চুদি", "fuck you", "xudi", "abal", " আবাল"]; // Dagdagan mo lang kung gusto mong maraming keywords 
   const warningText = "WARNING!";
 
   for (const keyword of sensitiveKeywords) {
